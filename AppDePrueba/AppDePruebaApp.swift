5361d0e6a6a53821feb0c38c55309116903da9fd
Raul <Raul>
2026-08-19 19:19:36 +0200

    Initial Commit

---
+++ AppDePrueba/AppDePruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDePruebaApp.swift
+//  AppDePrueba
+//
+//  Created by Raul Fernandez on 19/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppDePruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
